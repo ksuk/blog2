123850dee8a40398f5549e1e6950551ff6768eae
ksuk <keisukeniko7@gmail.com>
2018-12-09 18:22:59 +0900

    edit comment form

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
+    path("account/login/", views.LoginView, name="login"),
+    path("account/logout/", views.LogoutView, name="logout"),
 ]
